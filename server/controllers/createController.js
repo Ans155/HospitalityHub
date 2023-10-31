f1b6db74e543da0fc0155965b0fa4ce4b291004f
Ans155 <110165397+Ans155@users.noreply.github.com>
2023-11-01 02:59:30 +0530

    Create createController.js

---
+++ server/controllers/createController.js
@@ -0,0 +1,63 @@
+const Booking = require('../model/Booking');
+const Room = require('../model/Room');
+
+exports.bookRoom = async (req, res) => {
+  try {
+
+    const { userEmail, roomNumber, startTime, endTime, price } = req.body;
+
+    // Check if the room exists
+    const room = await Room.findOne({ roomNumber });
+
+    if (!room)
+    {
+      return res.status(404).json({ error: 'Room not found' });
+    }
+
+    // Check for overlapping bookings for the same room
+    const overlappingBooking = await Booking.findOne({
+      roomNumber,
+      $or: [
+        {
+          startTime: { $lte: startTime },
+          endTime: { $gte: startTime },
+        },
+        {
+          startTime: { $lte: endTime },
+          endTime: { $gte: endTime },
+        },
+      ],
+    });
+
+    if (overlappingBooking) {
+      return res.status(400).json({ error: 'Overlapping booking exists' });
+    }
+  //     const startDate = new Date(startTime);
+  // const endDate = new Date(endTime);
+
+  // // Calculate the time difference in milliseconds
+  // const timeDiff = endDate - startDate;
+
+  // // Calculate the number of hours
+  // const hours = timeDiff / (1000 * 60 * 60);
+    // Calculate the price based on the hourly rate and duration
+    // const hourlyRate = room.hourlyRate;
+    // const durationInHours = (endTime - startTime) / 1000 / 60 / 60;
+    // const price = hourlyRate * durationInHours;
+
+    // Create a new booking
+    const newBooking = new Booking({
+      userEmail,
+      roomNumber,
+      startTime,
+      endTime,
+      price,
+    });
+
+    await newBooking.save();
+    res.status(201).json(newBooking);
+  } catch (error) {
+    console.error('Error booking room:', error);
+    res.status(500).json({ error: 'Failed to book room' });
+  }
+};
