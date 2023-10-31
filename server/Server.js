9b55461c0a813e8cc13115e59a3c41cd5665a6ae
Ans155 <110165397+Ans155@users.noreply.github.com>
2023-11-01 03:00:00 +0530

    Update Server.js

--- server/Server.js
+++ server/Server.js
@@ -1,17 +1,17 @@
 require("dotenv").config(); //added
 const express = require("express");
-
 const connectDB = require("./config/db"); //added
-
+//const bookRoute = require('./routes/create'); // Import the route file
+const routes =require("./routes/create")
+const editRoute = require('./routes/edit');
 const app = express();
-
 // connect database
 connectDB();//added
-
 // initialize middleware
 app.use(express.json({ extended: false }));
-app.get("/", (req, res) => res.send("Server up and running"));
-
+//app.get("/", (req, res) => res.send("Server up and running"));
+app.use('/create', routes);
+app.use('/edit', editRoute);
 // setting up port
 const PORT = process.env.PORT || 5000;
 
