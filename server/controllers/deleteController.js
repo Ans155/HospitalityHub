707c2a00038226715265ef28ba743775e2e0354c
Ans155 <110165397+Ans155@users.noreply.github.com>
2023-11-01 19:15:59 +0530

    Create deleteController.js

---
+++ server/controllers/deleteController.js
@@ -0,0 +1,48 @@
+const Booking = require('../model/Booking');
+
+// Calculate the time difference between two dates in hours
+const calculateTimeDifferenceInHours = (date1, date2) => {
+  const diffInMs = date1 - date2;
+  return diffInMs / (1000 * 60 * 60);
+};
+
+// Delete a booking with refund conditions
+exports.deleteBooking = async (req, res) => {
+  try {
+    const { bookingId } = req.params;
+
+    // Find the booking by its bookingId
+    const booking = await Booking.findOne({ bookingId });
+
+    if (!booking) {
+      return res.status(404).json({ error: 'Booking not found' });
+    }
+
+    // Calculate the time difference in hours
+    const currentTime = new Date();
+    const startTime = new Date(booking.startTime);
+    const timeDifferenceInHours = calculateTimeDifferenceInHours(startTime, currentTime);
+
+    let refundPercentage = 0;
+
+    if (timeDifferenceInHours > 48) {
+      refundPercentage = 100; // Complete refund
+    } else if (timeDifferenceInHours >= 24 && timeDifferenceInHours <= 48) {
+      refundPercentage = 50; // 50% refund
+    }
+
+    // Perform the actual booking deletion from the database
+    await Booking.deleteOne({ bookingId });
+
+    // Prepare the response
+    const response = {
+      message: 'Booking deleted successfully',
+      refundPercentage,
+    };
+
+    res.status(200).json(response);
+  } catch (error) {
+    console.error('Error deleting booking:', error);
+    res.status(500).json({ error: 'Failed to delete booking' });
+  }
+};
