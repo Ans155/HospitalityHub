0736537f65fbc70555db48028da31beb296690d2
Ans155 <110165397+Ans155@users.noreply.github.com>
2023-11-02 04:01:44 +0530

    Update Server.js

--- server/Server.js
+++ server/Server.js
@@ -1,12 +1,16 @@
 require("dotenv").config(); //added
 const express = require("express");
 const connectDB = require("./config/db");
+const cors = require("cors");
 //const bookRoute = require('./routes/create'); // Import the route file
 const routes =require("./routes/create")
 const editRoute = require('./routes/edit');
 const deleteRoute = require("./routes/delete");
 const viewRoute = require("./routes/view");
+const viewRoomRoute = require("./routes/viewRoom");
+
 const app = express();
+app.use(cors());
 // connect database
 connectDB();//added
 // initialize middleware
@@ -16,6 +20,7 @@ app.use('/create', routes);
 app.use('/edit', editRoute);
 app.use('/delete', deleteRoute);
 app.use('/view', viewRoute);
+app.use('/viewRoom', viewRoomRoute);
 // setting up port
 const PORT = process.env.PORT || 5000;
 
