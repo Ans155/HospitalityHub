ea7e483519d15b8086114079f4ff46fac4924033
Ans155 <110165397+Ans155@users.noreply.github.com>
2023-11-01 19:16:07 +0530

    Create viewController.js

---
+++ server/controllers/viewController.js
@@ -0,0 +1,18 @@
+const Booking = require('../model/Booking');
+
+// View all bookings
+exports.viewBookings = async (req, res) => {
+  try {
+    const bookings = await Booking.find().exec();
+
+    if (!bookings || bookings.length === 0) {
+      // Handle the case when there are no bookings
+      return res.status(200).json({ bookings: [] });
+    }
+
+    res.status(200).json({ bookings });
+  } catch (error) {
+    console.error('Error fetching bookings:', error);
+    res.status(500).json({ error: 'Failed to fetch bookings' });
+  }
+};
