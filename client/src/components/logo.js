aaa6a876d3349b18a8b43a84b88b23ac991da80e
Ans155 <110165397+Ans155@users.noreply.github.com>
2023-12-14 15:00:55 +0530

    update

---
+++ client/src/components/logo.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const CustomLogo = () => {
+  return (
+    <svg width="20" height="20" xmlns="http://www.w3.org/2000/svg">
+      <circle cx="10" cy="10" r="9" fill="#007bff" />
+
+      
+      <text x="50" y="110" fill="white" fontSize="15" fontWeight="bold">
+      HospitalityHub
+      </text>
+    </svg>
+  );
+};
+
+export default CustomLogo;
