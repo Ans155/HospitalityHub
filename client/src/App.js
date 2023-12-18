7364bfd8d319df5e8501d268275ae9d50704dbad
Ans155 <110165397+Ans155@users.noreply.github.com>
2023-12-19 00:22:57 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,9 @@ import BookingForm from "./components/createBooking";
 import Signup from "./components/signup";
 import Login from "./components/Login";
 import UpdatingForm from "./pages/update";
-
+import RoomList from './components/viewRoom'
+import ReqBookings from "./pages/viewRequested"
+import MyBookings from './components/MyBookings'
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 function App() {
@@ -15,10 +17,13 @@ function App() {
         <ToastContainer />
         <Routes>
           <Route path="/view" element={<Bookings />} />
-          <Route path="/" element={<BookingForm />} />
+          <Route path="/requested" element={<ReqBookings />} />
+          <Route path="/create" element={<BookingForm />} />
           <Route path="/update" element={< UpdatingForm />} />
           <Route path="/signup" element={< Signup />} />
           <Route path="/login" element={< Login/>} />
+          <Route path="/rooms" element={< RoomList/>} />
+          <Route path="/user/bookedRooms" element={< MyBookings/>} />
           <Route index element={<Login />} />
         </Routes>
       </Router>
