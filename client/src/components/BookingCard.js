a1a6854c208c9da119035162427480feaad26dce
Ans155 <110165397+Ans155@users.noreply.github.com>
2023-12-19 00:22:00 +0530

    Update BookingCard.js

--- client/src/components/BookingCard.js
+++ client/src/components/BookingCard.js
@@ -1,9 +1,17 @@
 import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom";
+import { jwtDecode } from "jwt-decode";
+import axios from "axios";
+import { toast } from "react-toastify";
 
-function BookingCard({ booking }) {
+function BookingCard({ booking, page }) {
+  const token = localStorage.getItem('token');
+  const decodedToken = jwtDecode(token);
+  const userRole = decodedToken.role;
+
   function getDate(date) {
-    var options = {
+    const options = {
       weekday: "long",
       year: "numeric",
       month: "long",
@@ -12,13 +20,37 @@ function BookingCard({ booking }) {
     const newDate = new Date(date);
     return newDate.toLocaleDateString("en-US", options);
   }
+
   const newPrice = booking.price.toLocaleString("en-IN", {
     maximumFractionDigits: 2,
     style: "currency",
     currency: "INR",
   });
+
+  const handleConfirm = (id) => {
+    const config = {
+      method: "post",
+      maxBodyLength: Infinity,
+      url: `https://hotelbackend-4phi.onrender.com/create/${id}/validate`,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    axios(config)
+      .then(function (response) {
+        console.log(JSON.stringify(response.data));
+        if (response.data._id !== undefined) {
+          toast.success("Booking Confirmed and Confirmation email sent!");
+        } else toast.error("Booking Failed");
+      })
+      .catch(function (error) {
+        toast.error(error.response.data.message);
+      });
+  };
+
   return (
-    <Card className="w-100">
+    <Card className="w-100 mb-4">
       <Card.Body>
         <div className="row">
           <div className="col-md-6">
@@ -40,9 +72,21 @@ function BookingCard({ booking }) {
               <strong>Booking End Date: </strong>
               {`${getDate(booking.endTime)}`}
             </Card.Text>
-            <Link to="/update" state={booking}>
-              <button className="btn btn-secondary btn1 mr-auto">Update</button>
-            </Link>
+            <Card.Text>
+              <strong>Status: </strong>
+              {`${booking.status}`}
+            </Card.Text>
+            {userRole === 'admin' && page === 'requested' ? (
+              <Button variant="success" onClick={() => handleConfirm(booking.bookingId)}>
+                Confirm Booking
+              </Button>
+            ) : (
+              <Link to="/update" state={booking}>
+                <Button variant="secondary" className="mr-auto">
+                  Update
+                </Button>
+              </Link>
+            )}
           </div>
         </div>
       </Card.Body>
