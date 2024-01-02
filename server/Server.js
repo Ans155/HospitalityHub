520936401e053bf02c52e4fcacb7778621a4ca66
Ans155 <110165397+Ans155@users.noreply.github.com>
2024-01-02 13:20:48 +0530

    Update Server.js

--- server/Server.js
+++ server/Server.js
@@ -1,12 +1,10 @@
-require("dotenv").config(); //added
+const express = require("express");
+const cors = require("cors");
 const bodyParser = require('body-parser');
 const passport = require('./middleware/authMiddleware');
 const authRoutes = require('./routes/authRoutes');
-const express = require("express");
 const connectDB = require("./config/db");
-const cors = require("cors");
-//const bookRoute = require('./routes/create'); // Import the route file
-const routes =require("./routes/create")
+const routes = require("./routes/create");
 const editRoute = require('./routes/edit');
 const deleteRoute = require("./routes/delete");
 const viewRoute = require("./routes/view");
@@ -16,26 +14,31 @@ const userRoute = require("./routes/user");
 const statRoute = require("./routes/stats");
 
 const app = express();
-app.use(cors());
-// connect database
-connectDB();//added
-// initialize middleware
+
+require("dotenv").config();
+
+connectDB();
+
+app.use(cors({
+  origin: 'https://hospitalityhub.netlify.app/',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+}));
+
 app.use(express.json({ extended: false }));
-//app.get("/", (req, res) => res.send("Server up and running"));
 app.use(bodyParser.json());
+
 app.use('/auth', authRoutes);
 app.use('/create', routes);
 app.use('/edit', editRoute);
 app.use('/delete', deleteRoute);
-app.use('/view',viewRoute);
-app.use('/viewRoom',viewRoomRoute);
-app.use('/user',userRoute);
-app.use('/filter',filterRoute);
-app.use('/stats',statRoute);
-// setting up port
-const BASE_URL = process.env.BASE_URL;
-const PORT =process.env.PORT || 5000
+app.use('/view', viewRoute);
+app.use('/viewRoom', viewRoomRoute);
+app.use('/user', userRoute);
+app.use('/filter', filterRoute);
+app.use('/stats', statRoute);
 
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(`server is running on ${BASE_URL}`);
+  console.log(`Server is running on port ${PORT}`);
 });
