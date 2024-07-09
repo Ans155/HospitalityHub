c5aabc1b7b7ef355a52d94406539b07e09c2f799
ST-Ansh-Choudhary <anshchoudhary155@gmail.com>
2024-07-10 02:43:26 +0530

    build fixes

--- server/Server.js
+++ server/Server.js
@@ -1,60 +1,8 @@
 const express = require("express");
-const cors = require("cors");
-const bodyParser = require('body-parser');
-const passport = require('./middleware/authMiddleware');
-const authRoutes = require('./routes/authRoutes');
-const connectDB = require("./config/db");
-const routes = require("./routes/create");
-const editRoute = require('./routes/edit');
-const deleteRoute = require("./routes/delete");
-const viewRoute = require("./routes/view");
-const viewRoomRoute = require("./routes/viewRoom");
-const filterRoute = require("./routes/filter");
-const userRoute = require("./routes/user");
-const statRoute = require("./routes/stats");
-
 const app = express();
 
-require("dotenv").config();
+app.get("/", (req, res) => res.send("Express on Vercel"));
 
-connectDB();
+app.listen(3000, () => console.log("Server ready on port 3000."));
 
-app.use(cors({
-  origin: '*',
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true,
-  allowedHeaders: 'Content-Type,Authorization',
-}));
-
-app.use(express.json({ extended: false }));
-app.use(bodyParser.json());
-
-app.use('/auth', authRoutes);
-app.use('/create', routes);
-app.use('/edit', editRoute);
-app.use('/delete', deleteRoute);
-app.use('/view', viewRoute);
-app.use('/viewRoom', viewRoomRoute);
-app.use('/user', userRoute);
-app.use('/filter', filterRoute);
-app.use('/stats', statRoute);
-
-app.get('/keep-alive', (req, res) => {
-  res.status(200).send('Server is alive');
-});
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
-
-setInterval(() => {
-  const axios = require('axios');
-  axios.get('https://hotelbackend-4phi.onrender.com/keep-alive')
-    .then(response => {
-      console.log(response.data);
-    })
-    .catch(error => {
-      console.error('Error pinging keep-alive endpoint:', error.message);
-    });
-}, 300000);
+module.exports = app;
